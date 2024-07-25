61b30030ab10b6835bf6739e6411e07b2aa88e0a
manuel.cesar <manuel.cesar@unc.edu.ar>
2024-07-25 10:20:20 -0300

    Tab corrections

--- src/components/organizational-chart.vue
+++ src/components/organizational-chart.vue
@@ -15,195 +15,195 @@
 </template>
 
 <script>
-import OrganizationChart from 'vue-organization-chart'
-import 'vue-organization-chart/dist/orgchart.css'
-import records from '@/assets/records.json';
-import { roleTitles, titleColors } from '../utils/utils.js'
-export default {
-  components: {
-    OrganizationChart
-  },
-  data () {
-    return {
-      records,
-      agencyRecords: [],
-      roleTitles,
-      titleColors,
-    };
-  },
-  async created () {
-    this.agencyRecords = this.createChart(this.records);
-    
-  },
-  methods: {
-    createChart (records) {
-        const agency = {
-        id: 'agency',
-        name: 'Clients',
-        title: 'AGENCY',
-        children: []
-    };
-    const nodesMap = { agency }; 
-    records.forEach(record => {
-        let node;
+  import OrganizationChart from 'vue-organization-chart'
+  import 'vue-organization-chart/dist/orgchart.css'
+  import records from '@/assets/records.json';
+  import { roleTitles, titleColors } from '../utils/utils.js'
+  export default {
+    components: {
+      OrganizationChart
+    },
+    data () {
+      return {
+        records,
+        agencyRecords: [],
+        roleTitles,
+        titleColors,
+      };
+    },
+    async created () {
+      this.agencyRecords = this.createChart(this.records);
+      
+    },
+    methods: {
+      createChart (records) {
+          const agency = {
+          id: 'agency',
+          name: 'Clients',
+          title: 'AGENCY',
+          children: []
+      };
+      const nodesMap = { agency }; 
+      records.forEach(record => {
+          let node;
 
-        if (record.type.name === 'CLIENT') {
-            node = {
-                id: record.id,
-                name: record.name,
-                gender: record.gender,
-                image: record.image,
-                title: record.type.name,
-                children: [] 
-            };
-            agency.children.push(node);
-        }
+          if (record.type.name === 'CLIENT') {
+              node = {
+                  id: record.id,
+                  name: record.name,
+                  gender: record.gender,
+                  image: record.image,
+                  title: record.type.name,
+                  children: [] 
+              };
+              agency.children.push(node);
+          }
 
-        if (Array.isArray(record.accesses)) {
-            record.accesses.forEach(access => {
-                const userNode = {
-                    id: access.user.id,
-                    name: access.user.name,
-                    image: access.user.image,
-                    title: access.role.name,
-                    children: []
-                };
-                nodesMap[access.user.id] = userNode;
-                
-                if (node) { 
-                    node.children.push(userNode);
-                }
-            });
-        }
-    });
+          if (Array.isArray(record.accesses)) {
+              record.accesses.forEach(access => {
+                  const userNode = {
+                      id: access.user.id,
+                      name: access.user.name,
+                      image: access.user.image,
+                      title: access.role.name,
+                      children: []
+                  };
+                  nodesMap[access.user.id] = userNode;
+                  
+                  if (node) { 
+                      node.children.push(userNode);
+                  }
+              });
+          }
+      });
 
-    return agency;
+      return agency;
+      }
     }
   }
-}
 </script>
 
 <style>
 
-.org-node:hover  {
-    background-color: #dcedf7;
-    border-color: #0068ad;
-}
+  .org-node:hover  {
+      background-color: #dcedf7;
+      border-color: #0068ad;
+  }
 
-.org-node .avatar {
-  width: 100px;
-  height: 100px;
-  margin-bottom: 5px;
-  border-radius: 50%;
-  padding: auto;
-  border: 3px solid #b4b9d1;
-}
-
-.details {
-  margin-top: -10px;
-  text-align: center;
-  background-color: #fff;
-  border: 1px solid #b4b9d1;
-  border-radius: 5px;
-}
-
-.lines {
-  border: 1px solid black;
-}
-
-
-.details >h2 {
-  border-radius: 5px;
-  color: #fff;
-  padding: 2px 0px;
-  margin-bottom: 2px;
-}
-
-.details >p {
-    border-radius: 5px;
-    padding: 2px 0px;
+  .org-node .avatar {
+    width: 100px;
+    height: 100px;
     margin-bottom: 5px;
+    border-radius: 50%;
+    padding: auto;
+    border: 3px solid #b4b9d1;
   }
 
-.orgchart {
-  margin-top: 50px;
-  background: #f4fbff;
-  border-radius: 10px;
-  padding: 50px;
-}
+  .details {
+    margin-top: -10px;
+    text-align: center;
+    background-color: #fff;
+    border: 1px solid #b4b9d1;
+    border-radius: 5px;
+  }
 
-.orgchart .node:hover {
-    background-color: #f4fbff;
+  .lines {
+    border: 1px solid black;
+  }
+
+
+  .details >h2 {
+    border-radius: 5px;
+    color: #fff;
+    padding: 2px 0px;
+    margin-bottom: 2px;
+  }
+
+  .details >p {
+      border-radius: 5px;
+      padding: 2px 0px;
+      margin-bottom: 5px;
+    }
+
+  .orgchart {
+    margin-top: 50px;
+    background: #f4fbff;
     border-radius: 10px;
+    padding: 50px;
   }
 
-.orgchart-container {
-  position: relative;
-  display: inline-block;
-  height: 100vh; 
-  width: 100vw; 
-  border-radius: 5px;
-  overflow: auto;
-  text-align: center;
-}
+  .orgchart .node:hover {
+      background-color: #f4fbff;
+      border-radius: 10px;
+    }
 
-.orgchart .lines:nth-child(3) td {
-    height: 80px;
+  .orgchart-container {
+    position: relative;
+    display: inline-block;
+    height: 100vh; 
+    width: 100vw; 
+    border-radius: 5px;
+    overflow: auto;
+    text-align: center;
   }
 
-.orgchart .lines .rightLine {
-  border-right: 1px solid #0068ad;
-  float: none;
-  border-radius: 0;
-}
+  .orgchart .lines:nth-child(3) td {
+      height: 80px;
+    }
 
-.orgchart .lines .leftLine {
-  border-left: 1px solid #0068ad;
-  float: none;
-  border-radius: 0;
-}
+  .orgchart .lines .rightLine {
+    border-right: 1px solid #0068ad;
+    float: none;
+    border-radius: 0;
+  }
 
-.orgchart .lines .topLine {
-  border-top: 2px solid #0068ad;
-}
+  .orgchart .lines .leftLine {
+    border-left: 1px solid #0068ad;
+    float: none;
+    border-radius: 0;
+  }
 
-.orgchart .lines .downLine {
-  background-color: #0068ad;
-  height: 30px;
-  width: 3px;
-  float: none;
-}
+  .orgchart .lines .topLine {
+    border-top: 2px solid #0068ad;
+  }
 
-.level-1-title {
-    background-color: green;
-}
+  .orgchart .lines .downLine {
+    background-color: #0068ad;
+    height: 30px;
+    width: 3px;
+    float: none;
+  }
 
-.level-2-title {
-    background-color: blue;
-}
+  .level-1-title {
+      background-color: green;
+  }
 
-.admin-title {
-    background-color: red;
-}
+  .level-2-title {
+      background-color: blue;
+  }
 
-.client-title {
-    background-color: gray;
-}
+  .admin-title {
+      background-color: red;
+  }
 
-.agency-title {
-    background-color: blueviolet;
-}
+  .client-title {
+      background-color: gray;
+  }
 
-.orgchart .node {
-  width: 250px;
-}
+  .agency-title {
+      background-color: blueviolet;
+  }
 
-.org-node.PROVIDER_LEVEL_1 {
-  margin-top: 10em;
-}
+  .orgchart .node {
+    width: 250px;
+  }
 
-.org-node.PROVIDER_LEVEL_ADMIN {
-  margin-top: 20em;
-}
+  .org-node.PROVIDER_LEVEL_1 {
+    margin-top: 10em;
+  }
+
+  .org-node.PROVIDER_LEVEL_ADMIN {
+    margin-top: 20em;
+  }
 
 </style>
